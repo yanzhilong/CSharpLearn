b5daca81e3d0c85deeb86bba022fb824a24d1db7
yanzhilong <51877421@qq.com>
2017-01-19 00:22:08 +0800

    file learn

--- WindowsForms/FileForm.Designer.cs
+++ WindowsForms/FileForm.Designer.cs
@@ -31,6 +31,26 @@
             this.button1 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.filetb1 = new System.Windows.Forms.TextBox();
+            this.filetb2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.filetb = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button8 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.foldertb = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.SuspendLayout();
             // 
             // button1
@@ -61,17 +81,186 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // filetb1
+            // 
+            this.filetb1.Location = new System.Drawing.Point(5, 202);
+            this.filetb1.Name = "filetb1";
+            this.filetb1.Size = new System.Drawing.Size(100, 21);
+            this.filetb1.TabIndex = 3;
+            // 
+            // filetb2
+            // 
+            this.filetb2.Location = new System.Drawing.Point(164, 202);
+            this.filetb2.Name = "filetb2";
+            this.filetb2.Size = new System.Drawing.Size(100, 21);
+            this.filetb2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(128, 205);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "到";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(5, 242);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "复制";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(107, 242);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "移动";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(5, 272);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "删除文件及夹";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // filetb
+            // 
+            this.filetb.Location = new System.Drawing.Point(6, 332);
+            this.filetb.Name = "filetb";
+            this.filetb.Size = new System.Drawing.Size(109, 21);
+            this.filetb.TabIndex = 9;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(121, 330);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "选择文件";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(202, 330);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "查看信息";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(6, 302);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(99, 23);
+            this.button8.TabIndex = 12;
+            this.button8.Text = "遍历文件夹";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.Location = new System.Drawing.Point(6, 404);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(306, 100);
+            this.listView1.TabIndex = 16;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "文件名";
+            this.columnHeader1.Width = 70;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "路径";
+            this.columnHeader2.Width = 140;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "大小";
+            this.columnHeader3.Width = 40;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "创建日期";
+            // 
+            // foldertb
+            // 
+            this.foldertb.Location = new System.Drawing.Point(91, 377);
+            this.foldertb.Name = "foldertb";
+            this.foldertb.Size = new System.Drawing.Size(159, 21);
+            this.foldertb.TabIndex = 15;
+            this.foldertb.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 380);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "选择文件夹：";
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(262, 375);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(53, 23);
+            this.button9.TabIndex = 13;
+            this.button9.Text = "浏览";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
             // FileForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(276, 460);
+            this.ClientSize = new System.Drawing.Size(503, 580);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.foldertb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.filetb);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.filetb2);
+            this.Controls.Add(this.filetb1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button1);
             this.Name = "FileForm";
             this.Text = "FileForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -80,5 +269,25 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox filetb1;
+        private System.Windows.Forms.TextBox filetb2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox filetb;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.TextBox foldertb;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
     }
 }
--- WindowsForms/FileForm.cs
+++ WindowsForms/FileForm.cs
@@ -59,5 +59,126 @@ namespace WindowsForm
             }
             refreshMessage();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            File.Copy(filetb1.Text,filetb2.Text);
+            File.Copy(filetb1.Text, filetb2.Text,true);
+
+            FileInfo fileinfo = new FileInfo(filetb1.Text);
+            fileinfo.CopyTo(filetb2.Text);
+            fileinfo.CopyTo(filetb2.Text,true);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            File.Move(filetb1.Text, filetb2.Text);
+            FileInfo fileinfo = new FileInfo(filetb1.Text);
+            fileinfo.MoveTo(filetb2.Text);
+
+            Directory.Move(filetb1.Text,filetb2.Text);
+            DirectoryInfo directoryinfo = new DirectoryInfo(filetb1.Text);
+            directoryinfo.MoveTo(filetb2.Text);
+            //c:\\Test 到 d:\\新建文件夹\\Test
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            File.Delete(filetb1.Text);
+            FileInfo fileinfo = new FileInfo(filetb1.Text);
+            fileinfo.Delete();
+
+            //删除文件夹，只能删除空的文件夹
+            Directory.Delete(filetb1.Text);
+            Directory.Delete(filetb1.Text,true);//连子文件夹一起删除
+            DirectoryInfo directoryinfo = new DirectoryInfo(filetb1.Text);
+            directoryinfo.Delete();
+            directoryinfo.Delete(true);//连子文件夹一起删除
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filetb.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            FileInfo finfo = new FileInfo(filetb.Text);         //实例化FileInfo对象
+            string strCTime, strLATime, strLWTime, strName, strFName, strDName, strISRead;
+            long lgLength;
+            strCTime = finfo.CreationTime.ToShortDateString();    //获取文件创建时间
+            strLATime = finfo.LastAccessTime.ToShortDateString(); //获取上次访问该文件的时间
+            strLWTime = finfo.LastWriteTime.ToShortDateString();  //获取上次写入文件的时间
+            strName = finfo.Name;                                 //获取文件名称
+            strFName = finfo.FullName;                            //获取文件的完整目录
+            strDName = finfo.DirectoryName;                       //获取文件的完整路径
+            strISRead = finfo.IsReadOnly.ToString();              //获取文件是否只读
+            lgLength = finfo.Length;                              //获取文件长度
+            MessageBox.Show("文件信息：\n创建时间：" + strCTime + " 上次访问时间：" + strLATime + "\n上次写入时间：" + strLWTime + " 文件名称：" + strName + "\n完整目录：" + strFName + "\n完整路径：" + strDName + "\n是否只读：" + strISRead + " 文件长度：" + lgLength);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo directoryinfo = new DirectoryInfo(filetb1.Text);
+            DirectoryInfo[] dtiarray = directoryinfo.GetDirectories();//
+            DirectoryInfo[] dtiarray1 = directoryinfo.GetDirectories("搜索的名字");//
+            DirectoryInfo[] dtiarray2 = directoryinfo.GetDirectories("搜索的名字",SearchOption.TopDirectoryOnly);//当前目录
+            DirectoryInfo[] dtiarray3 = directoryinfo.GetDirectories("搜索的名字", SearchOption.AllDirectories);//所有子目录
+
+            FileInfo[] filearray = directoryinfo.GetFiles();//
+            FileInfo[] filearray1 = directoryinfo.GetFiles("搜索的名字");//
+            FileInfo[] filearray2 = directoryinfo.GetFiles("搜索的名字", SearchOption.TopDirectoryOnly);//当前目录的文件
+            FileInfo[] filearray3 = directoryinfo.GetFiles("搜索的名字", SearchOption.AllDirectories);//所有子目录的文件
+
+            FileSystemInfo[] filesysarray = directoryinfo.GetFileSystemInfos();//
+            FileSystemInfo[] filesysarray1 = directoryinfo.GetFileSystemInfos("搜索的名字");//
+
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                foldertb.Text = folderBrowserDialog1.SelectedPath;
+                //实例化DirectoryInfo对象
+                DirectoryInfo dinfo = new DirectoryInfo(foldertb.Text);
+                //获取指定目录下的所有子目录及文件类型
+                FileSystemInfo[] fsinfos = dinfo.GetFileSystemInfos();
+                foreach (FileSystemInfo fsinfo in fsinfos)
+                {
+                    if (fsinfo is DirectoryInfo)    //判断是否文件夹
+                    {
+                        //使用获取的文件夹名称实例化DirectoryInfo对象
+                        DirectoryInfo dirinfo = new DirectoryInfo(fsinfo.FullName);
+                        //为ListView控件添加文件夹信息
+                        listView1.Items.Add(dirinfo.Name);
+                        listView1.Items[listView1.Items.Count - 1].SubItems.Add(dirinfo.FullName);
+                        listView1.Items[listView1.Items.Count - 1].SubItems.Add("");
+                        listView1.Items[listView1.Items.Count - 1].SubItems.Add(dirinfo.CreationTime.ToShortDateString());
+                    }
+                    else
+                    {
+                        //使用获取的文件名称实例化FileInfo对象
+                        FileInfo finfo = new FileInfo(fsinfo.FullName);
+                        //为ListView控件添加文件信息
+                        listView1.Items.Add(finfo.Name);
+                        listView1.Items[listView1.Items.Count - 1].SubItems.Add(finfo.FullName);
+                        listView1.Items[listView1.Items.Count - 1].SubItems.Add(finfo.Length.ToString());
+                        listView1.Items[listView1.Items.Count - 1].SubItems.Add(finfo.CreationTime.ToShortDateString());
+                    }
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
